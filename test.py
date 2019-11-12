9cecc4000720df5955e5983f34f5fb2ee9edf793
kzhao27 <kzhao27@asu.edu>
2019-11-12 11:18:56 -0700

    1111

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import os
-subdir, dirs, existing_files = os.walk("./imagenet_data/tf_records/train/")
-
-print(len(existing_files))
-for file in existing_files:
-    print(file)
+for subdir, dirs, existing_files in os.walk("./imagenet_data/tf_records/train/"):
+    print(len(existing_files))
+    for file in existing_files:
+        print(file)
