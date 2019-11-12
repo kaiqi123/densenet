f78960656cdb2d083a9d6fc1402109cc83c02184
kzhao27 <kzhao27@asu.edu>
2019-11-12 11:17:17 -0700

    1111

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import os
-for subdir, dirs, files in os.walk("./imagenet_data/tf_records/train/"):
-    print(subdir, dirs, files)
+subdir, dirs, existing_files = os.walk("./imagenet_data/tf_records/train/")
+
+print(len(existing_files))
+for file in existing_files:
+    print(file)
