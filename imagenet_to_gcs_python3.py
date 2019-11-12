e65c5734cbf31bc307c90390981a20aadc030ec4
kzhao27 <kzhao27@asu.edu>
2019-11-11 22:29:28 -0700

    1111

--- imagenet_to_gcs_python3.py
+++ imagenet_to_gcs_python3.py
@@ -157,17 +157,17 @@ def _is_cmyk(filename):
   """
   # File list from:
   # https://github.com/cytsai/ilsvrc-cmyk-image-list
-  blacklist = set(['n01739381_1309.JPEG', 'n02077923_14822.JPEG',
-                   'n02447366_23489.JPEG', 'n02492035_15739.JPEG',
-                   'n02747177_10752.JPEG', 'n03018349_4028.JPEG',
-                   'n03062245_4620.JPEG', 'n03347037_9675.JPEG',
-                   'n03467068_12171.JPEG', 'n03529860_11437.JPEG',
-                   'n03544143_17228.JPEG', 'n03633091_5218.JPEG',
-                   'n03710637_5125.JPEG', 'n03961711_5286.JPEG',
-                   'n04033995_2932.JPEG', 'n04258138_17003.JPEG',
-                   'n04264628_27969.JPEG', 'n04336792_7448.JPEG',
-                   'n04371774_5854.JPEG', 'n04596742_4225.JPEG',
-                   'n07583066_647.JPEG', 'n13037406_4650.JPEG'])
+  blacklist = set([b'n01739381_1309.JPEG', b'n02077923_14822.JPEG',
+                   b'n02447366_23489.JPEG', b'n02492035_15739.JPEG',
+                   b'n02747177_10752.JPEG', b'n03018349_4028.JPEG',
+                   b'n03062245_4620.JPEG', b'n03347037_9675.JPEG',
+                   b'n03467068_12171.JPEG', b'n03529860_11437.JPEG',
+                   b'n03544143_17228.JPEG', b'n03633091_5218.JPEG',
+                   b'n03710637_5125.JPEG', b'n03961711_5286.JPEG',
+                   b'n04033995_2932.JPEG', b'n04258138_17003.JPEG',
+                   b'n04264628_27969.JPEG', b'n04336792_7448.JPEG',
+                   b'n04371774_5854.JPEG', b'n04596742_4225.JPEG',
+                   b'n07583066_647.JPEG', b'n13037406_4650.JPEG'])
   return os.path.basename(filename) in blacklist
 
 
